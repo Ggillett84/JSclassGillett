a7962707a8bf897b8804e87721eec898492ba034
Gloria Gillett <gloriagillett84@gmail.com>
2023-01-24 20:42:15 -0800

    Two JS files using module

--- 004 modularize/module1.js
+++ 004 modularize/module1.js
@@ -1,3 +1,12 @@
+/**
+ * comment
+ * in
+ * module
+ * js
+ * with
+ * block
+ * usually at top of page
+ */
 const myAlert = "My alert 1";
 const myConsole = "Text to console 2"; 
 export {myAlert, myConsole};
--- 004 modularize/module2.js
+++ 004 modularize/module2.js
@@ -1,3 +1,4 @@
+// comment in module js
 import { myAlert, myConsole } from "./module1.js";
 alert(myAlert);
 console.log(myConsole);
