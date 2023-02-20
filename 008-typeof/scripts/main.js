35df39bf3ef205f15ba28e4f93deb54e87fd35f9
Gloria Gillett <gloriagillett84@gmail.com>
2023-02-20 01:18:51 -0800

    my variable code

--- 008-typeof/scripts/main.js
+++ 008-typeof/scripts/main.js
@@ -1,23 +1,23 @@
 let myName = "Gloria"
 let myAge = 38
 let myMoney = true
-let newVar = 'SOMETHING'
+let thread = 'SOMETHING'
 const myConst = 42
-console.log(myAge == myName)
-console.log(myAge === myName)
+let myFood = "Epic"
+let everything = 42
 
-let MyVar = null
-console.log(`myVar is typeof ${typeof myVar}`)
+console.log(myConst === everything)
 
+let MyMind = null
+console.log(`myMind is typeof ${typeof myMind}`)
+console.log(`myFood is value ${myFood}`)
+console.log(`thread is typeof ${typeof thread}`)
 console.log(`myName is typeof ${typeof myName}`)
 console.log(`myName is value ${myName}`)
 console.log(`myAge is typeof ${typeof myAge}`)
 console.log(`myMoney is typeof ${typeof myMoney}`)
+console.log(`myConst is typeof ${typeof myConst}`)
 
-//dont use var
-//if (true) {
-//    const myConst = 43
-//  console.log(myConst)
-//}else{
-//
-//}
+
+
+//I had already played while watching the video. sorry
