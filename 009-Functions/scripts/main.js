97cf2b18134c947b37d418d7ee30155cd775a1d0
Gloria Gillett <gloriagillett84@gmail.com>
2023-02-20 01:56:22 -0800

    fun with fumctions

---
+++ 009-Functions/scripts/main.js
@@ -0,0 +1,11 @@
+//Functions are fun!
+
+function doMath (a, b){
+    //let result = a
+    console.log (` this is the result ${a-b}`)
+    console.log (` this is the result ${a+b}`)
+    console.log (` this is the result ${a/b}`)
+    console.log (` this is the result ${a*b}`)
+}
+
+doMath (10, 4)
