dfffcedc7f1b6ec8659acbeec3066a9263584c44
Gloria Gillett <gloriagillett84@gmail.com>
2023-02-20 00:55:46 -0800

    more on variables

---
+++ 008-typeof/scripts/main.js
@@ -0,0 +1,23 @@
+let myName = "Gloria"
+let myAge = 38
+let myMoney = true
+let newVar = 'SOMETHING'
+const myConst = 42
+console.log(myAge == myName)
+console.log(myAge === myName)
+
+let MyVar = null
+console.log(`myVar is typeof ${typeof myVar}`)
+
+console.log(`myName is typeof ${typeof myName}`)
+console.log(`myName is value ${myName}`)
+console.log(`myAge is typeof ${typeof myAge}`)
+console.log(`myMoney is typeof ${typeof myMoney}`)
+
+//dont use var
+//if (true) {
+//    const myConst = 43
+//  console.log(myConst)
+//}else{
+//
+//}
