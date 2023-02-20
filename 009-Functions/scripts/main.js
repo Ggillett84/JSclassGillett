285ad385ec5a80a9bda32143240afd6b0c66dcb7
Gloria Gillett <gloriagillett84@gmail.com>
2023-02-20 02:11:45 -0800

    return of functions

--- 009-Functions/scripts/main.js
+++ 009-Functions/scripts/main.js
@@ -1,11 +1,13 @@
 //Functions are fun!
 
-function doMath (a, b){
-    //let result = a
-    console.log (` this is the result ${a-b}`)
-    console.log (` this is the result ${a+b}`)
-    console.log (` this is the result ${a/b}`)
-    console.log (` this is the result ${a*b}`)
+function doMath (a, b) {
+    let result = a * b
+    return result
+   // console.log (` this is the result ${a-b}`)
+   // console.log (` this is the result ${a+b}`)
+   // console.log (` this is the result ${a/b}`)
+    //console.log (` this is the result ${a*b}`)
 }
 
-doMath (10, 4)
+ let result = doMath(10, 4)
+ console.log(`this result was returned from the function ${result}`)
