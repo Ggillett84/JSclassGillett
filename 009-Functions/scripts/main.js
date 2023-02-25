2e1a0e08b2f34e6887b3b5ecd3e5e1df5d510a38
Gloria Gillett <gloriagillett84@gmail.com>
2023-02-24 18:19:39 -0800

    fun with functions

--- 009-Functions/scripts/main.js
+++ 009-Functions/scripts/main.js
@@ -1,11 +1,15 @@
 //Functions are fun
 
 function doMath (a, b) {
-    
-    console.log(`This is the result ${a / b}`)
-    console.log(`This is the result ${a * b}`)
-    console.log(`This is the result ${a + b}`)
-    console.log(`This is the result ${a - b}`)
+    let result = a * b
+    return result
+// console.log(`This is the result ${a / b}`)
+//    console.log(`This is the result ${a * b}`)
+//    console.log(`This is the result ${a + b}`)
+//    console.log(`This is the result ${a - b}`)
 }
 
-doMath(10, 4) 
+
+
+let returnValue = doMath(10, 4)
+console.log (`This result was returned from the function ${returnValue}`)
