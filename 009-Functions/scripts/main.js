acab84ead9afbbc44a920e6d12bc5bbefa754a82
Gloria Gillett <gloriagillett84@gmail.com>
2023-02-26 16:48:28 -0800

    arrow functions

--- 009-Functions/scripts/main.js
+++ 009-Functions/scripts/main.js
@@ -1,25 +1,45 @@
 //My functions
 
-function biWeek (a, b, c) {
-    let result = a * b * c
+function doMath (a, b) {
+    let result = a * b
     return result
+    //console.log (`This is the result ${a - b}`)
+    //console.log (`This is the result ${a + b}`)
+    //console.log (`This is the result ${a / b}`)
+    //console.log (`This is the result ${a * b}`)
 }
 
-let returnValue = biWeek(20.19, 8, 10)
-console.log (`Bi-wekkly Gross income ${returnValue}`)
+doMoreMath = (a, b) => a * b
 
-function monthly (a, b){
-    let result = a *b
-    return result
-}
+console.log (doMoreMath (20, 30))
 
-let returnResult = monthly (1615.2, 2)
-console.log (`Monthly Gross income ${returnResult}`)
 
-function mBills (a, b, c, d, e){
-    let result = a + b + c + d
-    return result
-}
 
-let returnSum = mBills (1200, 100, 110, 280, 120,)
-console.log (`My monthly bills ${returnSum}`)
+
+const result = doMath (10, 4)
+console.log(`This result was returned from the function ${result}`)
+
+
+
+//function biWeek (a, b, c) {
+//let result = a * b * c
+//return result
+//}
+
+//let returnValue = biWeek(20.19, 8, 10)
+//console.log (`Bi-wekkly Gross income ${returnValue}`)
+
+
+//converted function
+monthly = (a, b) => a * b
+
+console.log (monthly (1615.2, 2))
+
+
+//function mBills (a, b, c, d, e){
+//    let result = a + b + c + d
+//   return result
+//}
+
+//let returnSum = mBills (1200, 100, 110, 280, 120,)
+//console.log (`My monthly bills ${returnSum}`)
