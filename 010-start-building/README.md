2e254f9dd56c72927dd775a86f78c3f180a3d57e
Gloria Gillett <gloriagillett84@gmail.com>
2023-03-03 14:33:59 -0800

    learning about arrays

--- 010-start-building/README.md
+++ 010-start-building/README.md
@@ -1,1 +1,6 @@
+# Are you using devTools?
 Yes I am using the devTools. So far I am not really getting it, but I am learning every time. I know it will be helpful in the future, so I will not give up. 
+
+# Does this Array concept make sense to you?
+
+Yes it does. I am going to need to play with it a bit more, but I can see it working with devToold and that helps a lot. 
--- 010-start-building/scripts/main.js
+++ 010-start-building/scripts/main.js
@@ -1,5 +1,7 @@
 //Update the DOM
 
+const myArr = []
+
 const updateDOM = (input) => {
     const divEl = document.querySelector('#output')
     const p = document.createElement('p')
@@ -7,12 +9,36 @@ const updateDOM = (input) => {
     divEl.appendChild(p)
 }
 
-const mileage = (miles, gallons, price = 3.79) => miles/gallons * price
+const trackMPGCost = (miles, gallons, price = 3.79) => {
+   const MPG = miles/gallons
+   const tripCost = MPG * price
+   myArr.push(MPG, tripCost)
+}
 
+updateDOM (trackMPGCost (300, 10, 5.40))
+updateDOM (trackMPGCost (320, 12, 5))
 
-updateDOM (mileage (300, 10, 5.40))
 
 updateDOM('output to DOM')
+
+
+//my code using arrays
+const myBudget = []
+
+const budget = (rent, pge, internet, income) => {
+const bills = rent + pge + internet
+const leftOver = income - bills
+myBudget.push (bills, leftOver)
+}
+
+updateDOM (budget(1200, 289, 110, 3300))
+
+
+const kids =['Izaiah', 'Gia', 'Zenith', 'Tyleah']
+kids.sort()
+console.log(kids)
+
+
 
 // My mileage function
 const trip = (miles, gallons, price, food) => miles/gallons * price + food
