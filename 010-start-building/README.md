03387658d6d0ba7507b9ec9ce6e5e1ab6497878e
Gloria Gillett <gloriagillett84@gmail.com>
2023-03-03 20:49:26 -0800

    fixing issues

--- 010-start-building/README.md
+++ 010-start-building/README.md
@@ -4,3 +4,8 @@ Yes I am using the devTools. So far I am not really getting it, but I am learnin
 # Does this Array concept make sense to you?
 
 Yes it does. I am going to need to play with it a bit more, but I can see it working with devToold and that helps a lot. 
+
+# Did you noticed there was NO update to the DOM on the code after I included the array example????  First see if you can fix the code to have it update the DOM! Were you successful? Also did notice that my tripCost was not correct? Were you able to fix these issues?
+
+I did notice that it was not updated to the DOM, but I thought...oh maybe its just a code for the console... 
+I was not able to fix it on my own, but once i went along with you I was able to see that I had mistakes in my milage function. I did not notice that your trip cost was not correct, darn, I usually catch these kind of things. I ran my code and eveything is correct now. Thank you!
--- 010-start-building/scripts/main.js
+++ 010-start-building/scripts/main.js
@@ -10,14 +10,15 @@ const updateDOM = (input) => {
 }
 
 const trackMPGCost = (miles, gallons, price = 3.79) => {
-   const MPG = miles/gallons
-   const tripCost = MPG * price
+   const MPG = Math.round(miles/gallons)
+   const tripCost = gallons * price
+   updateDOM (`miles per gallon ${MPG} and trip cost is ${tripCost}`)
    myArr.push(MPG, tripCost)
 }
 
-updateDOM (trackMPGCost (300, 10, 5.40))
-updateDOM (trackMPGCost (320, 12, 5))
-
+trackMPGCost (300, 10, 5.40)
+trackMPGCost (320, 12, 5)
+updateDOM(myArr)
 
 updateDOM('output to DOM')
 
@@ -25,25 +26,31 @@ updateDOM('output to DOM')
 //my code using arrays
 const myBudget = []
 
-const budget = (rent, pge, internet, income) => {
-const bills = rent + pge + internet
-const leftOver = income - bills
-myBudget.push (bills, leftOver)
+    const budget = (rent, pge, internet, income) => {
+    const bills = rent + pge + internet
+    const leftOver = income - bills
+    myBudget.push (bills, leftOver)
+    updateDOM(`Money leftover after bills are paid ${leftOver}`)
 }
 
-updateDOM (budget(1200, 289, 110, 3300))
+(budget(1200, 289, 110, 3300))
 
 
 const kids =['Izaiah', 'Gia', 'Zenith', 'Tyleah']
 kids.sort()
-console.log(kids)
+updateDOM(`My babies in alphabetical order ${kids}`)
 
 
 
 // My mileage function
-const trip = (miles, gallons, price, food) => miles/gallons * price + food
+const trip = (miles, gallon, price, food) => {
+    const MPG = (miles/gallon)
+    const gasPrice = (MPG * price)
+    const totalCost = (gasPrice + food)
+    updateDOM (`Total cost of my trip is ${totalCost}`)
+}
 
-updateDOM (trip (450, 15, 4.80, 140))
+trip(450, 15, 4.80, 140)
 
 // my DOM changes
 const myDOMupdate = (input) => {
