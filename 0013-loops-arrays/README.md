287cfc2c6210d3e14c47e85194948f81ca5644ed
Gloria Gillett <gloriagillett84@gmail.com>
2023-03-07 17:37:19 -0800

    refactor and feedback

---
+++ 0013-loops-arrays/README.md
@@ -0,0 +1,8 @@
+# week 7 loops and arrays
+I have been using the DevTools and I am getting more and more every time, but I feel like I should know more. I have been able to find my mistakes and I have been able to follow along with the step by step function in the debugger mode. But the little side things like the scope and call stack I am still getting to understand. 
+
+I feel like I am struggling with it. I am able to find my mistakes and correct them, but I am not sure I can do this without help. I was so good with HTML and CSS, so this is really bringing me down. i hope its normal to feel like this. 
+on the last video i got really confused, but the code DID look better. I am having trouble learning more than one way at a time. I know there ARE more than one way to do this stuff, but for my learning abbilities, I am not getting a chance to grasp any concept. 
+Is the "i" for indext the only letter you can use?
+whats the diffrence when you put "const calculateSUM = (arr)" V. "const calculateSUM = ()"
+I had more questions, some I answered as I was learning along with you, others I forgot. I need to just jump in the README and put them in so I dont forget.
--- 0013-loops-arrays/scripts/main.js
+++ 0013-loops-arrays/scripts/main.js
@@ -1,5 +1,5 @@
-const myMPG = []
-const myTripCost =[]
+const MY_MPG = []
+const MY_TRIP_COST =[]
 
 const updateDOM = (input) => {
     const divEl = document.querySelector('#output')
@@ -12,37 +12,40 @@ const trackMPGandCost = (miles, gallons, price = 3.79) => {
    const MPG = Math.round(miles/gallons)
    const tripCost = Math.round (gallons * price)
    updateDOM (`miles per gallon ${MPG} and trip cost is ${tripCost}`)
-   myMPG.push(MPG)
-   myTripCost.push(tripCost)
+   MY_MPG.push(MPG)
+   MY_TRIP_COST.push(tripCost)
 }
 
 const calculateAvgMPG = () => {
     let totalMPG = 0    
-    for(let i = 0; i < myMPG.length; i++) {
-        totalMPG = totalMPG + myMPG [i]
+    for(let i = 0; i < MY_MPG.length; i++) {
+        totalMPG = totalMPG + MY_MPG [i]
     }
-    let avgMPG = totalMPG / myMPG.length
+    let avgMPG = totalMPG / MY_MPG.length
     updateDOM(`Average MPG is ${avgMPG}`)
 }
 
 //const calculateAvgCost = () => {
 //   let totalTripCost = 0
-//    for(let i = 0; i < myTripCost.length; i++) {
-//       totalTripCost = totalTripCost + myTripCost [i]
+//    for(let i = 0; i < MY_TRIP_COST.length; i++) {
+//       totalTripCost = totalTripCost + MY_TRIP_COST [i]
 //    }
-//    let avgCost = totalTripCost / myTripCost.length
+//    let avgCost = totalTripCost / MY_TRIP_COST.length
 //   updateDOM (`Average trip cost is ${avgCost}`)
 //}
 
-const calculateAvg = () => {
-    let sumMPG = 0
-    let sumTripCost = 0
-    for(let i = 0; i < myMPG.length; i++) {
-        sumMPG = math.round(sumMPG + myMPG [i])
-        sumTripCost = math.round(sumTripCost + myTripCost [i])
+const calculateSUM = (arr) => {
+    let sum = 0
+    for(let i=0; i < arr.length; i++) {
+    sum = sum + arr [i]
     }
-    let avgMPG = sumMPG / myMPG.length
-    let avgTripCost = sumTripCost /myTripCost.length
+    return sum
+}
+const calculateAvg = () => {
+    let sumMPG = calculateSUM(MY_MPG)
+    let sumTripCost = calculateSUM(MY_TRIP_COST)
+    let avgMPG = Math.round (sumMPG / MY_MPG.length)
+    let avgTripCost = Math.round (sumTripCost /MY_TRIP_COST.length)
     updateDOM (`Average trip cost is ${avgTripCost}`)
     updateDOM (`Average MPG is ${avgMPG}`)
 }
