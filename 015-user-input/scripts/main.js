e2268e6cc72405a312da0724ab6b17b2b77c8d45
Gloria Gillett <gloriagillett84@gmail.com>
2023-03-12 15:23:41 -0700

    form HTML

---
+++ 015-user-input/scripts/main.js
@@ -0,0 +1,44 @@
+const MY_MPG = []
+const MY_TRIP_COST =[]
+
+const updateDOM = (input) => {
+    const divEl = document.querySelector('#output')
+    const p = document.createElement('p')
+    p.textContent = input
+    divEl.appendChild(p)
+}
+
+const trackMPGandCost = (miles, gallons, price = 3.79) => {
+   const MPG = Math.round(miles/gallons)
+   const tripCost = Math.round (gallons * price)
+   updateDOM (`miles per gallon ${MPG} and trip cost is ${tripCost}`)
+   MY_MPG.push(MPG)
+   MY_TRIP_COST.push(tripCost)
+}
+
+const calculateSUM = (arr) => {
+    let sum = 0
+
+   for(value of arr) {
+    sum += value
+   }
+   return sum
+}
+
+const calculateAvg = () => {
+    let sumMPG = calculateSUM(MY_MPG)
+    let sumTripCost = calculateSUM(MY_TRIP_COST)
+    let avgMPG = Math.round (sumMPG / MY_MPG.length)
+    let avgTripCost = Math.round (sumTripCost /MY_TRIP_COST.length)
+    updateDOM (`Average trip cost is ${avgTripCost}`)
+    updateDOM (`Average MPG is ${avgMPG}`)
+}
+
+
+trackMPGandCost (360, 15, 5.40)
+trackMPGandCost (320, 12, 5)
+trackMPGandCost (100, 15, 5.40)
+trackMPGandCost (600, 24, 5.70)
+trackMPGandCost (50, 2, 3.40)
+trackMPGandCost (320, 12, 5)
+calculateAvg()
