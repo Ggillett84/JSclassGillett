03dad03dd6098663aa507b973ff2b3d92b225815
Gloria Gillett <gloriagillett84@gmail.com>
2023-03-25 20:13:51 -0700

    this is mine

--- 017-intro-to-objects/scripts/main.js
+++ 017-intro-to-objects/scripts/main.js
@@ -20,3 +20,17 @@ const myObj = {
 
 const greeting = myObj.myGreeting ('Kelly')
 console.log (greeting)
+
+const myFam = {
+    me: 'Gloria',
+    husband: 'Kelly',
+    kids: ['Izaiah', 'Gia', 'Tyleah', 'Zenith'],
+    myOther: function (him) {
+        return `${this.me} is married to, and loves ${him}. We have 4 kids; ${this.kids}. 
+        Our oldest is ${this.kids[0]} at 23 years old, and our youngest is ${this.kids[3]}
+        at 3 years old}. Our crazy teen ${this.kids[2]} is musically inclined (she made me put this in).`
+    }
+}
+
+const intro = myFam.myOther ('Kelly')
+console.log (intro)
