69e26982ca8b7bc6117146e06c98e87e2868c4b4
Gloria Gillett <gloriagillett84@gmail.com>
2023-04-26 21:53:52 -0700

    my decision logic

--- 100-my-app/scripts/main.js
+++ 100-my-app/scripts/main.js
@@ -1,3 +1,23 @@
-const typeOfRestarant = Sushi
-const openTime = 4
-const kids = false
+const updateDOM = (input) => {
+    const divEl = document.querySelector('#output')
+}
+
+const extraMoney = true;
+const openTime = 4;
+const kids = false;
+
+// extraMoney ()
+// openTime ()
+// kids ()
+
+if (kids === false && openTime <= 4) {
+    if (extraMoney === true){
+        console.log (`We are going to Sakanaya`)
+    }else{
+        console.log (`We are going to Wasabi on Fire`)
+    }
+}else {
+    console.log (`We are staying home today`)
+} 
+
+
