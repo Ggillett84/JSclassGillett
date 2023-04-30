ce41b527cadcd032b1355df968805cce57241f7c
Gloria Gillett <gloriagillett84@gmail.com>
2023-04-30 01:18:35 -0700

    step 1 without working code.

--- 100-my-app/scripts/main.js
+++ 100-my-app/scripts/main.js
@@ -1,23 +1,17 @@
-const updateDOM = (input) => {
-    const divEl = document.querySelector('#output')
-}
-
 const extraMoney = true;
 const openTime = 4;
 const kids = false;
-
+const carWorking = "yes";
 // extraMoney ()
 // openTime ()
 // kids ()
 
 if (kids === false && openTime <= 4) {
-    if (extraMoney === true){
-        console.log (`We are going to Sakanaya`)
-    }else{
-        console.log (`We are going to Wasabi on Fire`)
-    }
-}else {
-    console.log (`We are staying home today`)
-} 
-
-
+  if (extraMoney === true && carWorking === "yes") {
+    console.log(`We are going to Sakanaya`);
+  } else {
+    console.log(`We are going to Wasabi on Fire`);
+  }
+} else {
+  console.log(`We are staying home today`);
+}
