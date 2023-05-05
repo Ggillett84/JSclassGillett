3b52faf0a63f2cff0909272f7cc625416f7af3ac
Gloria Gillett <gloriagillett84@gmail.com>
2023-05-05 03:03:02 -0700

    student subclass

--- 027-intro-to-oop/README.md
+++ 027-intro-to-oop/README.md
@@ -1,1 +1,5 @@
 I see that it separates the objecs from the function. You can create prototype to work with in your code. the class lets multiple objects that can sher the same functions. 
+
+# "student subclass"
+
+I am kind of getting it, but I know I need to practice with it. 
--- 027-intro-to-oop/scripts/main.js
+++ 027-intro-to-oop/scripts/main.js
@@ -9,7 +9,17 @@ class Person {
     }
 }
 
-const gloria = new Person ("gloria", "Gillett", "123 Mountain way")
+class Student extends Person{
+    constructor (firstName, lastName, address, classList){
+    super (firstName, lastName, address)
+    this.classList = classList
+}
+showStatus (){
+    console.log (` ${this.firstName} lves at ${this.address}, and is taking ${this.classList}`)
+}
+}
+
+const gloria = new Student ("gloria", "Gillett", "123 Mountain way", ["Eng 1A", "CIT93"])
 gloria.showStatus ()
 const kelly = new Person ("kelly", "Gillett", "123 Mountain Way")
 kelly.showStatus ()
