6f0fa6e0ef12dacea3472393826cca49d2825d91
Gloria Gillett <gloriagillett84@gmail.com>
2023-05-15 01:24:32 -0700

    async code

--- 029-async-code/main.js
+++ 029-async-code/main.js
@@ -1,11 +1,49 @@
-let pizza
+// let pizza
 
-function  orderPizza () {
-    console.log ('Order Pizza')
-    setTimeout (() => {
-    pizza = '🍕'
- }, 2000)
-    console.log ('Pizza was ordered')
+// function  orderPizza () {
+//     console.log ('Order Pizza')
+//     setTimeout (() => {
+//     pizza = '🍕'
+//     console.log (`${pizza} is ready`)
+//  }, 2000)
+//     console.log ('Pizza was ordered')
+// }
+// orderPizza()
+// console.log ('Call Qoli')
+// console.log (`Eat ${pizza}`)
+
+function orderPizza (callback) {
+    setTimeout (()=> {
+        const pizza = '🍕'
+        callback(pizza)
+    },2000)
 }
-orderPizza()
-console.log (`Eat ${pizza}`)
+
+function pizzaReady (pizza) {
+    console.log (`Eat the  ${pizza}`)
+}
+orderPizza (pizzaReady)
+console.log('call Qoli') 
+
+window.addEventListener ('click', callback)
+function callback (){
+    console.log ('Clicked')
+}
+
+// function thing1 (callback){
+//     callback()
+// }
+// function thing2 (callback){
+//     callback()
+// }
+// function thing3 (callback){
+//     callback()
+// }
+
+// thing1(() => {
+//     thing2(() => {
+//         thing3 (() => {
+
+//         })
+//     })
+// })
