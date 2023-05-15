2da2ecad6c8456568e0a0eea007b54f11fd9f394
Gloria Gillett <gloriagillett84@gmail.com>
2023-05-15 02:21:52 -0700

    coded along

--- 030-async-workout/README.md
+++ 030-async-workout/README.md
@@ -3,3 +3,6 @@
 I actuall have no idea and was going to ask why they were all the same and if it would confuse the code or show an error because it has already been declared....
 
 I thought I caould just use the code from the 029 file and input the data I needed, Im sure I have it all mixed up. but I tried. 
+
+Wow that was so cool. I can see this being used as a time out thing for kids. sorry I have a 3 year old and she is on the spectrum and very smart. I can see that it could be set up as a time out for ( ) because the reason is very important to her her age ( ) because they say 1 min per year, and a visual along with an audio stop notification because that what she is waiting for. 
+not sure if that what you meant by how can we use this, but that where my brain went. 
--- 030-async-workout/main.js
+++ 030-async-workout/main.js
@@ -1,29 +1,25 @@
 const formEl = document.getElementById('form')
 const output = document.getElementById('output')
-const updateDom = (callback)
 
-function type (callback){
-     callback(type)
- }
- function reps (callback){
-     callback(reps)
- }
- function time (callback){   
-     callback(time)
- }
- 
- type((updateDom) => {
-     reps((updateDom) => {
-         time ((updateDom) => {
- 
-         })
-     })
- })
+
+function updateDOM(message, el) {
+    const newEl = document.createElement(el)
+    newEl.textContent = message
+    output.appendChild(newEl)
+}
+
+function startWorkout (type, reps, time, fn) {
+    fn(`Start ${type} Goal reps is ${reps}`, 'p')
+    setTimeout(()=> {
+        fn(`Stop ${type}`, 'h1')
+    }, time * 1000)
+}
 
 formEl.addEventListener('submit', function(e) {
     e.preventDefault();
     const type = e.target.type.value
     const reps = parseFloat(e.target.reps.value)
     const time = parseFloat(e.target.time.value)
+    startWorkout (type, reps, time, updateDOM)
     formEl.reset()
 })
