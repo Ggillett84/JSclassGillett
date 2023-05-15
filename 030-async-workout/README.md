68cb2aa0f85d7d602ac767ba4f47ba7aa2041333
Gloria Gillett <gloriagillett84@gmail.com>
2023-05-15 02:05:02 -0700

    my code

--- 030-async-workout/README.md
+++ 030-async-workout/README.md
@@ -1,3 +1,5 @@
 #  what are the call backs for each callback
 
 I actuall have no idea and was going to ask why they were all the same and if it would confuse the code or show an error because it has already been declared....
+
+I thought I caould just use the code from the 029 file and input the data I needed, Im sure I have it all mixed up. but I tried. 
--- 030-async-workout/main.js
+++ 030-async-workout/main.js
@@ -1,5 +1,24 @@
 const formEl = document.getElementById('form')
 const output = document.getElementById('output')
+const updateDom = (callback)
+
+function type (callback){
+     callback(type)
+ }
+ function reps (callback){
+     callback(reps)
+ }
+ function time (callback){   
+     callback(time)
+ }
+ 
+ type((updateDom) => {
+     reps((updateDom) => {
+         time ((updateDom) => {
+ 
+         })
+     })
+ })
 
 formEl.addEventListener('submit', function(e) {
     e.preventDefault();
