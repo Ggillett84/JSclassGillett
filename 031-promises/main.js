5a5ee1cf14a1d88c0106c50a149c843b90524d86
Gloria Gillett <gloriagillett84@gmail.com>
2023-05-17 12:03:39 -0700

    playing with ChatGPT.

--- 031-promises/main.js
+++ 031-promises/main.js
@@ -1,40 +1,40 @@
-function getWeather (){
-    return new Promise (function(resolve, reject){
-        setTimeout(()=>{
-            resolve ('Sunny')
-        },100)
-    })
-}
+// function getWeather (){
+//     return new Promise (function(resolve, reject){
+//         setTimeout(()=>{
+//             resolve ('Sunny')
+//         },100)
+//     })
+// }
 
-function getWeatherIcon (weather){
-    return new Promise (function(resolve, reject){
-        setTimeout(()=>{
-            switch(weather){
-                case 'Sunny': 
-                resolve ('☀️')
-            break 
-                 case 'Cloudy':
-                 resolve('⛅')  
-            break
-                case 'Raining':
-                resolve('🌧️')   
-            default:   
-                reject ('NO ICON FOUND')
+// function getWeatherIcon (weather){
+//     return new Promise (function(resolve, reject){
+//         setTimeout(()=>{
+//             switch(weather){
+//                 case 'Sunny': 
+//                 resolve ('☀️')
+//             break 
+//                  case 'Cloudy':
+//                  resolve('⛅')  
+//             break
+//                 case 'Raining':
+//                 resolve('🌧️')   
+//             default:   
+//                 reject ('NO ICON FOUND')
                 
-        }
-     },100)
-  })
-}
+//         }
+//      },100)
+//   })
+// }
 
-function onSuccess (data){
-    console.log(`Success ${data}`)
-}
+// function onSuccess (data){
+//     console.log(`Success ${data}`)
+// }
 
-function onError (error){
-    console.log(`Error: ${data}`)
-}
+// function onError (error){
+//     console.log(`Error: ${data}`)
+// }
 
-getWeather()
-.then (getWeatherIcon)
-.then(onSuccess, onError)
+// getWeather()
+// .then (getWeatherIcon)
+// .then(onSuccess, onError)
  
---
+++ 035-chatgpt/README.md
@@ -0,0 +1,9 @@
+# 5 different prompts
+
+1. explain a function in JavaScript 
+2. what are some good JavaScript workout for beginners 
+3. explain API interactions in JavaScript 
+4. How often should I do JavaScript workouts to understand it better
+5. can you explain promises in JavaScript 
+
+I love how much it expalins in its answers. It is very helpful and I can see it helping me further understand JS. I do like how you mentioned that it could help you with other things too. I thought it was just for programming. I really like this program. 
